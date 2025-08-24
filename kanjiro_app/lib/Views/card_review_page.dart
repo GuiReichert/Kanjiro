79bdbb1766d278acb29a02c2e642f6de1b5b8d97
Guilherme Reichert <guireichert.tr@gmail.com>
2025-08-24 02:13:35 -0300

    Creating Card Review Page

--- kanjiro_app/lib/Views/card_review_page.dart
+++ kanjiro_app/lib/Views/card_review_page.dart
@@ -1,14 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:kanjiro_app/Widgets/background_claro_widget.dart';
 
-class CardReviewPage extends StatelessWidget {
+class CardReviewPage extends StatefulWidget {
   const CardReviewPage({super.key});
+
+  @override
+  State<CardReviewPage> createState() => _CardReviewPageState();
+}
+
+class _CardReviewPageState extends State<CardReviewPage> {
+  bool mostrarResposta = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: KanjiroBackgroundClaro(widgetFilho: Container()),
-      bottomNavigationBar: BottomAppBar(),
+      body: KanjiroBackgroundClaro(
+        widgetFilho: Center(
+          child: Container(
+            decoration: BoxDecoration(color: Colors.white.withOpacity(0.7)),
+            child: Center(
+              child: Text(
+                'KANJI AQUI',
+                style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ),
+      ),
+      appBar: barraSuperiorCardReview(),
+      bottomNavigationBar: BottomAppBar(
+        color: const Color.fromARGB(255, 10, 40, 68).withOpacity(0.8),
+        child: mostrarResposta ? botoesDificuldade() : botaoMostrarResposta(),
+      ),
+    );
+  }
+
+  Center botaoMostrarResposta() {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          setState(() {
+            mostrarResposta = true;
+          });
+        },
+        child: Text('Mostrar Resposta'),
+      ),
+    );
+  }
+
+  Row botoesDificuldade() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        ElevatedButton(onPressed: () {}, child: Text('Easy')),
+        ElevatedButton(onPressed: () {}, child: Text('Normal')),
+        ElevatedButton(onPressed: () {}, child: Text('Hard')),
+      ],
+    );
+  }
+
+  AppBar barraSuperiorCardReview() {
+    return AppBar(
+      backgroundColor: const Color.fromARGB(255, 10, 40, 68).withOpacity(0.8),
+      elevation: 1,
+      leadingWidth: 500,
+      leading: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            child: Text('Voltar', style: TextStyle(color: Colors.white)),
+            alignment: Alignment.centerLeft,
+            margin: EdgeInsets.only(left: 10),
+          ),
+          SizedBox(width: 300),
+          Container(
+            child: Text('Opcões', style: TextStyle(color: Colors.white)),
+          ),
+        ],
+      ),
     );
   }
 }
